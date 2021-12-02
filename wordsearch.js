3e90b47f7a9f44bc223934d2421fe69f54a43fd0
Adrian Kiva <akiva@mta.ca>
2021-12-01 20:02:34 -0500

    debug template and add vetical functionality through TDD in pair programming with  @Veronica Galii

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    if (letters.length === 0) {return undefined}
+    const verticalJoin = letters[0].map((ls, index) => {
+        let verticalWord = ""
+        for (let i = 0; i < letters.length; i++) {
+            verticalWord += letters[i][index]
+        } 
+        return verticalWord;
+    });
+    for (let wordVertical of verticalJoin) {
+        if (wordVertical.includes(word)) 
+        return true
     }
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    for (let wordHorizontal of horizontalJoin) {
+        if (wordHorizontal.includes(word)) 
+        return true
+    }
+    //create new words using the index of the first array
+    return false
 }
 
+
+
 module.exports = wordSearch
+
+// console.log(
+//     wordSearch(
+//         [], "ho"
+//     )
+// )
